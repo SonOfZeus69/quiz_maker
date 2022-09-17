9a62367f0d77dfbee39c11731c0e884a23cd6975
Babajide <sobowaleheyzeez@gmail.com>
2022-09-17 04:53:50 +0100

    Firebase Authentication

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,7 +1,9 @@
+import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 
 Widget inputFile({
   label,
+  disclaimer,
   obscureText = false,
 }) {
   return Column(
@@ -18,8 +20,11 @@ Widget inputFile({
       const SizedBox(
         height: 5,
       ),
-      TextField(
+      TextFormField(
         obscureText: obscureText,
+        validator: (String? val) {
+          return val!.isEmpty ? disclaimer : null;
+        },
         decoration: const InputDecoration(
           contentPadding: EdgeInsets.symmetric(
             vertical: 0,
@@ -39,3 +44,30 @@ Widget inputFile({
     ],
   );
 }
+
+void showFloatingFlushbar(BuildContext context, String text) {
+    Flushbar(
+      duration: const Duration(seconds: 5),
+      margin: const EdgeInsets.all(8),
+      padding: const EdgeInsets.all(10),
+      backgroundGradient: LinearGradient(
+        colors: [Colors.blue.shade800, Colors.blueAccent.shade700],
+        stops: const [0.6, 1],
+      ),
+      boxShadows: const [
+        BoxShadow(
+          color: Colors.black45,
+          offset: Offset(3, 3),
+          blurRadius: 3,
+        ),
+      ],
+      // All of the previous Flushbars could be dismissed by swiping down
+      // now we want to swipe to the sides
+      dismissDirection: FlushbarDismissDirection.HORIZONTAL,
+      // The default curve is Curves.easeOut
+      forwardAnimationCurve: Curves.fastLinearToSlowEaseIn,
+      flushbarPosition: FlushbarPosition.TOP,
+      title: 'Success',
+      message: text,
+    ).show(context);
+  }
